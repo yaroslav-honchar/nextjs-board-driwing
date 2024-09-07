50a80f3bcf543e51e334b841feeb8a50da001d38
Yaroslav Honchar <yaroslav.honchar.1@gmail.com>
2024-09-07 16:48:12 +0300

    feat: create organization dialog

--- app/(dashboard)/_components/sidebar/components/button-new.tsx
+++ app/(dashboard)/_components/sidebar/components/button-new.tsx
@@ -3,24 +3,26 @@
 import { PlusIcon } from "lucide-react"
 import React from "react"
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
+import { CreateOrganization } from "@clerk/nextjs"
 
 export const ButtonNew: React.FC = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <div className={"aspect-square"}>
+        <div className="aspect-square">
           <button
-            className={
-              "bg-white/25 size-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition"
-            }
-            type={"button"}
-            aria-label={"Create new"}
+            className="bg-white/25 size-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition"
+            type="button"
+            aria-label="Create new"
+            aria-expanded="false"
           >
-            <PlusIcon className={"size-5 text-white"} />
+            <PlusIcon className="size-5 text-white" />
           </button>
         </div>
       </DialogTrigger>
-      <DialogContent></DialogContent>
+      <DialogContent className="p-0 bg-transparent border-none max-w-[30rem]">
+        <CreateOrganization />
+      </DialogContent>
     </Dialog>
   )
 }
