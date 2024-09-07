c66f8b80d527431fae7c086c0887d3ffff781c93
Yaroslav Honchar <yaroslav.honchar.1@gmail.com>
2024-09-07 12:11:50 +0300

    feat: added button component; users with catch user id page

---
+++ app/users/[userId]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import type { IPageProps } from "./page.props"
+
+const UserIdPage: React.FC<IPageProps> = () => {
+  return <>User id page</>
+}
+
+export default UserIdPage
